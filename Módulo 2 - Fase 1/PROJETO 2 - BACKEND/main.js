434015644de52f8ace787500b439f60a0592ae7c
lucaszioto <lucaszioto@gmail.com>
2022-10-04 20:26:00 -0300

    n2

--- Módulo 2 - Fase 1/PROJETO 2 - BACKEND/main.js
+++ Módulo 2 - Fase 1/PROJETO 2 - BACKEND/main.js
@@ -1,8 +1,10 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 
-app.get('/tasks', () => {
+app.get('/tasks', (request, response) => {
     console.log('Entrei nas tarefas')
+    response.json({message: 'Ola mundo', teste: '123'})
 })
 
 app.listen(3333, () => {
